c4ee7fdc8489293e0a676ace8fad4affd009feaf
Nour Eid <noorhamdyeid19@gmail.com>
2025-04-30 11:01:11 +0300

    chore: replace process.env.JWT_SECRET with imported JWT_SECRET in checkAuth middleware

--- src/middlewares/checkAuth.ts
+++ src/middlewares/checkAuth.ts
@@ -1,11 +1,12 @@
 import * as jwt from "jsonwebtoken";
+import { JWT_SECRET } from "../config/config";
 
 export const checkAuth = (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
   if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
   }
-  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+  jwt.verify(token, JWT_SECRET, (err, user) => {
     if (err) {
       return res.status(403).json({ message: "Invalid token" });
     }
